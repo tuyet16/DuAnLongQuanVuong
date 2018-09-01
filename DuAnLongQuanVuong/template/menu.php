0596872f7c46de97ae76dcfbd7e0c174c664a468
Tuyet Nguyen <tuyetnguyentn3@gmail.com>
2018-09-01 21:46:42 +0700

    tuyetgui

--- DuAnLongQuanVuong/template/menu.php
+++ DuAnLongQuanVuong/template/menu.php
@@ -39,26 +39,16 @@ ob_start(); //Bien luu = chuoi
 </div>
     <nav class="navbar navbar-inverse navbar-fixed-top" style="background-color: darkblue;color: yellow;border: 1px solid darkblue;">
       <div class="container">
-<<<<<<< HEAD
         <ul class="nav navbar-nav">
           <li class="dropdown">
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> 7f5699dc970c62f2edc0fbcd9025088f3bf9ad2b
         
         <ul class="nav navbar-nav">
          <!-- <li><a href="home_controller.php">Trang Chủ</a></li>-->
           <li class="dropdown">
-<<<<<<< HEAD
-=======
->>>>>>> ffdaddfe9c46bbf0240bb60b008339356d3de549
         <div class="navbar-header">
            <a class="navbar-brand" href="home_controller.php" style="color: white;">Trang Chủ</a>
            
         </div>
->>>>>>> 7f5699dc970c62f2edc0fbcd9025088f3bf9ad2b
         <ul class="nav navbar-nav">
            <!--<li><a href="#">Trang Chủ</a></li>-->
          <li class="dropdown">
